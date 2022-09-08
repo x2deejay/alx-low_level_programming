2aa75b0b03bf44aedb19b2d91b6025c32ec297e2
Joshua <008danieljoshua@gmail.com>
2022-09-08 23:40:17 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - with proper grammar, but the outcome is a piece of art
+ *
+ * Decription:Prints with proper grammar, but the outcome is a piece of art
+ * Return:0
+ */
+int main(void)
+{
+char z[100] = "with proper grammer, but the outcome is a piece of art,";
+printf("%s", z); 
+return (0);
+}
