a0cc66cee168de314c8b063e344f8ddee7bb5dae
Joshua <008danieljoshua@gmail.com>
2022-09-08 23:05:34 +0000

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 /**
  * puts - This a function that prints 'Programming is line building a multiligual puzzle'
  *
  * @output_string: Programming is like building a multilingual puzzle
  *
- * Return: Always o (Succrss)
+ * Return: 0
  */
-int puts(char str)
-int main()
+int main(void)
 {
-    char output_words="Programming is like building a multilingual puzzle"
-    puts(output_words);
+    puts("\") Programming is like building a mmultilingual language");
     return (0);
-
 }
