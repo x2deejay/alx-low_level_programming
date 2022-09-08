a2d110dbc2878e0d676f62dadf3ecfcca74f01ec
Joshua <008danieljoshua@gmail.com>
2022-09-08 22:28:13 +0000

    Assembeler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #!/bin/bash
-char puts("Programming is like building a multilingual puzzle")
+/**
+ * puts - This a function that prints 'Programming is line building a multiligual puzzle'
+ *
+ * @output_string: Programming is like building a multilingual puzzle
+ *
+ * Return: Always o (Succrss)
+ */
+char puts(char output_string)
+{
+    output_string="Programming is like building a multilingual puzzle";
+}
