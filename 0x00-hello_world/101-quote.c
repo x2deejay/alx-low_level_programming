a4b08d46241dde9554ba53083678452b8edb5406
Joshua <008danieljoshua@gmail.com>
2022-09-09 02:00:57 +0000

    Quote edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - print some string
+ *
+ * Description: prints and that piece of art is useful
+ * Return: 0
+ */
+int main(void)
+{
 fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
+}
+
