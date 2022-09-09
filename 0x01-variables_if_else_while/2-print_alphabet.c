764679c4bd280c585caa5b5446796a05e1be231a
Joshua <008danieljoshua@gmail.com>
2022-09-09 20:55:06 +0000

    Prints out alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Mian entry point
+ *
+ * Description: Pints out all lower case
+ * @n: lower case input
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+	n = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+	putchar (n);
+	return (0);
+}
