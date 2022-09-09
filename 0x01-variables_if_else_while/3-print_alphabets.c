6016d7ce3b5e1fd906f2418b1a9bd32a72e39d6b
Joshua <008danieljoshua@gmail.com>
2022-09-09 22:40:41 +0000

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *  main - Main entry point
+ *
+ *  Description: Outputs letters
+ *  Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ *
+ * Description: outputs upper case
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+	
