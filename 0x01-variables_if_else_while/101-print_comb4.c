e746f31146067bd24997050fb43a12a54b1add52
Joshua <008danieljoshua@gmail.com>
2022-09-10 01:59:22 +0000

    Three digit combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ *
+ * Description: This program through the entry point
+ * runs all possible combinations of three digits
+ * Return: 0
+ */
+
+void printTwoCombinations(int a, int b, int c)
+{
+	putchar(a+'0'); putchar(b+'0'); putchar(c+'0'); putchar('\n');
+	putchar(a+'0'); putchar(c+'0'); putchar(b+'0'); putchar('\n');
+}
+int main(void)
+{ 
+	int i, j, last;
+	for (i = 0; i < 3; ++i)
+	{
+		for (j = 0; j < 3; ++j)
+		{
+			if (j==i) continue;
+			last = j+1;
+			if (i==last) ++last;
+			printTwoCombinations(i,j,last);
+		}
+		break;
+	}
+	return 0;
+}	
