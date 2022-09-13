15a89f4c58baafa5072f7a8b984be06743b04066
Joshua <008danieljoshua@gmail.com>
2022-09-13 21:11:51 +0000

    Changed the directory

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *main - main entry point
+ *
+ *Decription: This program prints all lower case alphabets
+ *abcdefghijklmnopqrstuvwxyz
+ *Return: void
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
