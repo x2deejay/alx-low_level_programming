035972c4c1b991c5264b13c098b1863ffb9d4302
Joshua <008danieljoshua@gmail.com>
2022-09-14 17:25:49 +0000

    98 relative

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ *
+ * Description: This function prints natural numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+}
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
+	}
+}
