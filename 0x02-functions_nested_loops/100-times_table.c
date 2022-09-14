faa9c2a35cbeb381d3d92cb69c10e3fe720d8ac8
Joshua <008danieljoshua@gmail.com>
2022-09-14 18:16:48 +0000

    some addy

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * main - main entry point
+ *
+ * Description: prints the time table
+ * @n: Integer for which the times table
+ * * will be printed
+ */
+
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
+}
+
+void print_times_table(int n)
+{
+	int row, column, product;
+
+	if (n >= 0 && n < 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				product = row * column;
+				if (column == 0)
+				{
+					_putchar('0');
+				}
+				else if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product > 99 && product < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product / 10 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ *
+ * Decription: 
+ *
+ */
+
+int sum_of_multiples(void)
+{
+	int num1;
+	int num2;
+	int i;
+	int j;
+
+	num1 = 3;
+	num2 = 5;
+	i = 3;
+	j = 5;
+	while (i < 1024)
+	{
+		int a;
+
