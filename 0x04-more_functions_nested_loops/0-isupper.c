36b87163307f7c30c76738ea5c3d589adef36480
Joshua <008danieljoshua@gmail.com>
2022-09-15 23:29:47 +0000

    Using chmod

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,17 +5,6 @@
  * Return: 0
  */
 
-int main(void)
-{
-	char c;
-
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
-}
-
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
