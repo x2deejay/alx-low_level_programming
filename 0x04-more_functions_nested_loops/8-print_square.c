48168f5c51c05189693f541e5f692c54ca30fec1
Joshua <008danieljoshua@gmail.com>
2022-09-16 19:04:05 +0000

    Square commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,33 +6,25 @@
  * Return: 0
  */
 
-int main(void)
-{
-	print_square(2);
-	print_square(10);
-	print_square(0);
-	return (0);
-}
-
 void print_square(int size)
 {
 	int row;
 	int col;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (size > 0)
 	{
 		for (row = 0 ; row < size; row++)
 		{
-			for (col = 0; col < (size -1); col++)
+			for (col = 0; col < (size - 1); col++)
 			{
 				_putchar('#');
 			}
 			_putchar('#');
 			_putchar('\n');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
