e73aa089841b5c1ba0b7fc8fbccf26b4aaf68716
Joshua <008danieljoshua@gmail.com>
2022-09-16 22:23:52 +0000

    some edits

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
 
 	for (n1 = 0; n1 < 10; n1++)
 	{
-		for (n2 = 0; n2 < 14; n2++)
+		for (n2 = 0; n2 <= 14; n2++)
 		{
-			if (n2 < 9)
+			if (n2 > 9)
 			{
 				_putchar((n2 / 10) + '0');
 			}
