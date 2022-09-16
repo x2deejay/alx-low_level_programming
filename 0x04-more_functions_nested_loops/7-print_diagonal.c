401c5032d15d4ee2eabc569f4f614bd73ad55a89
Joshua <008danieljoshua@gmail.com>
2022-09-16 16:27:39 +0000

    Print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,10 +8,23 @@
 
 void print_diagonal(int n)
 {
-	while (n-- > 0)
+	int len;
+	int space;
+
+	if (n > 0)
 	{
-		_putchar('\\');
-		_putchar('\n');
+		for (len = 0; len < n; n++)
+		{
+			for (space = 0; space < len; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			if (len == (n - 1))
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
 	}
-	return (0);
 }
