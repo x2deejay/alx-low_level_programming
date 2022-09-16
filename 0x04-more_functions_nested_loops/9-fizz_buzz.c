fdcc42c1c6a8a1425a67bcc25754b236eb3c783e
Joshua <008danieljoshua@gmail.com>
2022-09-16 22:46:30 +0000

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	int num;
 
-	num = 1;
-	while (num <= 100)
+	num = 0;
+	while (num++ < 100)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
 		{
@@ -34,7 +34,6 @@ int main(void)
 		{
 			printf("%d ", num);
 		}
-		num++;
 	}
 	printf("\n");
 	return (0);
