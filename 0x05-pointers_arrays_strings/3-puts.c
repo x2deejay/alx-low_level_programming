d7f3b11a7e7a4a5a3bbe23f0413f48e062bfb918
Joshua <008danieljoshua@gmail.com>
2022-09-19 18:54:21 +0000

    puts edited

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -13,7 +13,7 @@ void _puts(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		_putchar(stri[i]);
+		_putchar(str[i]);
 		i++;
 	}
 	_putchar('\n');
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @str: Input string pointer
+ * Return: void
+ */
+
+void print_rev(char *str)
+{
+	int j;
+	int i;
+
+	j = 0;
+	while (j != '\0')
+	{
+		j++;
+	}
+
+	i = 0;
+	while (i <= j)
+	{
+			
+
+
