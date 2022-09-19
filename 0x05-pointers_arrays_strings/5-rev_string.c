f7456bce36d8b75615eff49c33d17c1c3e2d8766
Joshua <008danieljoshua@gmail.com>
2022-09-19 22:35:47 +0000

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * rev_string - prints string in reverse
+ * @s: Input string pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int count;
+	int i;
+	char temp;
+	int middle;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	middle = count / 2;
+	for (i = 0; i < middle; i++)
+	{
+		temp = s[i];
+		s[i] = s[count - i - 1];
+		s[count - i - 1] = temp;
+	}
+	printf("\n");
+
+
+}
