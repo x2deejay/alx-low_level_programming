075d863d770635c5cfd7028ff9d6603f5f47479b
Joshua <008danieljoshua@gmail.com>
2022-09-20 15:15:32 +0000

    print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,12 +16,12 @@ void print_array(int *a, int n)
 	{
 		if (inc != n - 1)
 		{
-			printf("%d, ", a[inc]);
+			_putchar(a[inc] + ',');
 		}
 		else
 		{
-			printf("%d", a[inc]);
+			_putchar(a[inc]);
 		}
 	}
-	_putchar(10);
+	_putchar("\n");
 }
