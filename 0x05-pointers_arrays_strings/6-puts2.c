3d17d54effc106cfa6ba7447c3d45fb5955f22a2
Joshua <008danieljoshua@gmail.com>
2022-09-20 00:46:03 +0000

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,12 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (*(str + i) != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
-	}
-	_putchar('\n');
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+		i++;
 }
 
