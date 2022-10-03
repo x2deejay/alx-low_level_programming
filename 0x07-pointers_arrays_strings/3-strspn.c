d28a897e286122f031f5fd55eb110a2d52a02e7b
Joshua <008danieljoshua@gmail.com>
2022-10-03 21:33:37 +0000

    Commit for strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: array to find substring from
+ * @accept: prefix substring parameter
+ * Return: substring length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	j = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (*(accept + j) == *(s + i))
+			{
+				break;
+			}
+			if (!accept[j])
+			{
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (j);
+}
