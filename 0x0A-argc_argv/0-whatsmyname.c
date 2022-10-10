e8ebafb0bfea8d5cc93ef1dd8b23b8af5ead3068
Joshua <008danieljoshua@gmail.com>
2022-10-10 08:13:20 +0000

    whatsmyname commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - main entry point
@@ -9,6 +11,9 @@
 
 int main(int argc, char *argv[])
 {
-	_putchar(argv[0]);
+	if (argc != '\0')
+	{
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }
