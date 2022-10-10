3298c5c4d8ae069ac60af77f7073f237521cbe30
Joshua <008danieljoshua@gmail.com>
2022-10-10 11:14:59 +0000

    1-arfs commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ * @argc: auguements count
+ * @argv: arguements pointer
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv[0])
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
