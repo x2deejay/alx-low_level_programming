fefa3114bf527c6cc6c329f141f860f050d9fbb2
Joshua <008danieljoshua@gmail.com>
2022-10-16 23:57:34 +0000

    first struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - fisrt struct definition
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: This is my first struct definition in c
+ * programming
+ */
+
 struct dog
 {
 	char *name;
